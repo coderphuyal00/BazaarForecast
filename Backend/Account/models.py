f7615b9b86a5312802efa8e1ac325e9f564587b6
lucifervenom00 <unique.myself10402906@gmail.com>
2025-10-03 16:28:51 +0545

    Fix all errors and issues

--- Backend/Account/models.py
+++ Backend/Account/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUserModel(AbstractUser):
     full_name=models.CharField(max_length=50,null=True,blank=True)
     email=models.EmailField(unique=True)
+    # username=None
     REQUIRED_FIELDS=[]
     def __str__(self):
         return self.email
