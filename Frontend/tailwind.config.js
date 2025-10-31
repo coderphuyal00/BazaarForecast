98756b3f4e97eadaf0fc98e6c24b700ee45b1827
lucifervenom00 <unique.myself10402906@gmail.com>
2025-11-01 00:48:01 +0545

    Minor Updates

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,22 +1,37 @@
-import { Config } from "tailwindcss";
-
-import { mtConfig } from "@material-tailwind/react";
+// tailwind.config.js
 const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
-  content: [],
-  theme: {
-    extend: {
-      keyframes: {
-        slideLeft: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-50%)" },
+const { mtConfig } = require("@material-tailwind/react");
+module.exports = withMT(
+ mtConfig({
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+         animationPlayState: ['hover', 'group-hover'],
+        keyframes: {
+          slideLeft: {
+            "0%": { transform: "translateX(0)" },
+            "100%": { transform: "translateX(-50%)" },
+          },
+        },
+        animation: {
+         
+          slideLeft: "slideLeft 10s linear infinite",
         },
       },
-      animation: {
-        slideLeft: "slideLeft 10s linear infinite",
-      },
     },
+    plugins: [require("tailwindcss-animate"),
+      function ({ addUtilities }) {
+    addUtilities({
+      ".animation-pause": { "animation-play-state": "paused" },
+    }, ["responsive", "hover", "group-hover"]);
   },
-  plugins: [],
-});
+    ],
+  })
+);
+
